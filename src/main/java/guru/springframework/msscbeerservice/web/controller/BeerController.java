2daefaf2b973caa76d6ce445d1b1e8b88b4e4d04
Yerkebulan Yessenali <yerkebulanyessenali@gmail.com>
2023-06-13 11:29:38 +0600

    add controller tests

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +23,15 @@ public class BeerController {
 	}
 	
 	@PostMapping
-	pubilc ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+	public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
 		//todo impl
 		return new ResponseEntity(HttpStatus.CREATED);
 	}
 	
+	@PutMapping("/{beerId}")
+	public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+		//todo impl
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
 	
 }
